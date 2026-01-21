8dca97159ce474f899a383a21b3da1de7502654a
Applelyyy <islandgameming@gmail.com>
2026-01-21 14:33:51 +0700

    AAA

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,23 @@
 // Write your code below
+
+
+const n =Number(process.argv[2]);
+if (!isNaN(n) && n >0) {
+    for (let i = 1; i <= n; i++) {
+        let line = '';
+        
+        // This inner loop prints spaces
+        for (let s = 1; s <= n - i; s++) { 
+          line += ' '; 
+        }
+        
+        // This inner loop prints number
+        for (let j = 1; j <= i; j++) {
+          line += j;
+        }
+        console.log(line);
+      }
+}
+
+
+
--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,11 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+function getUniqueNumbers(arr1: number[], arr2: number[]):number[] {
+  const mix = arr1.concat(arr2)
+  const result = mix.filter((item,i,ar,) => ar.indexOf(item) !== i)
+  return (result)
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+
+
+console.log(getUniqueNumbers(arr1,arr2))
