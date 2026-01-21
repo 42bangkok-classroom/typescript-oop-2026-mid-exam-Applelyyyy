e655437a66e017255c8ede5f70f9d441738b91c3
Applelyyy <islandgameming@gmail.com>
2026-01-21 16:12:14 +0700

    AAA

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,9 @@
 export function getUniqueNumbers(arr1: number[], arr2: number[]):number[] {
   const mix = arr1.concat(arr2)
   const result:number[] = mix.filter((item,i,ar,) => ar.indexOf(item) !== i)
+  if(!result){
+    return []
+  }
   return result
 }
 
